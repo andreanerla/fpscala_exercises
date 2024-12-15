dc2fdf8d433de26fdd5eecdd0d63e97dc2209bb4
Andrea Nerla <andreanerla@Andreas-MacBook-Pro.local>
2024-12-15 18:46:47 +0100

    attempting 2_2

--- 2/2_2.scala
+++ 2/2_2.scala
@@ -1,3 +1,15 @@
 def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
-  println("prova")
-  }
+  @annotation.tailrec
+  var cnt = 0
+  if (cnt == length(as)) {
+    return ordered(as[cnt - 1], as[cnt])
+    }
+  else {
+    println("intheloop")
+    var cnt = cnt + 1 
+    println(cnt)
+    isSorted(as, ordered)
+  }  
+}
+
+isSorted(Array(1, 3, 5, 7), (x: Int, y: Int) => x < y)
